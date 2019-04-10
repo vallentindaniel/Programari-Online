d436634a36d7fcfbcffe92715415b62aa908f232
vallentindaniel <49476775+vallentindaniel@users.noreply.github.com>
2019-04-10 12:57:14 +0300

    Test
    Test

--- config.php
+++ config.php
@@ -10,6 +10,6 @@ define('DB_NAME', 'tfop');
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 // Check connection
 if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+    die("ERROR: Could not connect. Fraiere" . mysqli_connect_error());
 }
 ?>
